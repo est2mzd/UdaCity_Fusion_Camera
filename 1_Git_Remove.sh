2744e0a55323b9e465358fc9402e561c17ddd799
est2mzd <est2mzd@gmail.com>
2022-03-09 10:16:49 +0900

    add .sh

---
+++ 1_Git_Remove.sh
@@ -0,0 +1,9 @@
+git rm -rf --cached ./L04_05_Harris_Corner_Detection/cornerness_harris/build
+git rm -rf --cached ./L04_05_Harris_Corner_Detection/cornerness_harris/cmake-build-debug
+
+git rm -rf --cached ./L04_06_NMS_Algorithm/NMS/cmake-build-debug
+git rm -rf --cached ./L04_07_Popular_Keypoint_Detectors/detect_keypoints/cmake-build-debug
+
+git rm -rf --cached ./L04_11_HOG_vs_BinaryDescriptors/describe_keypoints/cmake-build-debug
+
+git rm -rf --cached ./L04_12_Descriptor_Matching/descriptor_matching/cmake-build-debug
